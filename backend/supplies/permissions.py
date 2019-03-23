7d33cf19af494359fc074f365ce5462ee653590f
Marek Pokropiński <marek.pokropinski@outlook.com>
2019-03-23 15:25:54 +0100

    Changed permission class that was inherited to permissions.BasePermission

--- backend/supplies/permissions.py
+++ backend/supplies/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 from .models import Supply
 
 
-class IsAuthenticatedReadOnly(permissions.IsAdminUser):
+class IsAuthenticatedReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated and request.method in permissions.SAFE_METHODS:
             return True
