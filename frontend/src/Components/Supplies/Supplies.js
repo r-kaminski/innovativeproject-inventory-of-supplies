a1f05346bfcdb69a04f7db3242f13c3ccf831ae1
Adrian Dobrzanski <avion.ms@live.com>
2019-03-27 23:49:40 +0100

    Add mui-datatables with dependencies and implement sample table with mock data

--- frontend/src/Components/Supplies/Supplies.js
+++ frontend/src/Components/Supplies/Supplies.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import MUIDataTable from "mui-datatables";
 import styles from './Supplies.module.css';
 
 class Supplies extends Component{
+    columns = ["Name", "Company", "City", "State"];
+
+    data = [
+    ["Joe James", "Test Corp", "Yonkers", "NY"],
+    ["John Walsh", "Test Corp", "Hartford", "CT"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL"],
+    ["James Houston", "Test Corp", "Dallas", "TX"],
+    ];
+    
+    options = {
+    filterType: 'checkbox',
+    };
+
     render(){
         //console.log(styles.pagewrapper)
         return(
-            <div class={styles.wrapper}>
+            <div className={styles.wrapper}>
                 <header>
                     STOCK
                 </header>
-                <div class={styles.tablePlaceholder}>
-                    TABELA
-                </div>
+                <MUIDataTable
+                        className={styles.table}
+                        title={"Employee List"}
+                        data={this.data}
+                        columns={this.columns}
+                        options={this.options} />
             </div>            
         );
-    }
+    };
 }
 
 export default Supplies;
