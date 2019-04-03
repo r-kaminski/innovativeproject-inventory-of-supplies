a427eee2ab30b4e28c84125c4c68cb2e321ec997
Hubert-Komputer <hwnukiewicz@gmail.com>
2019-04-03 15:53:17 +0200

    Changes according to code review

--- frontend/src/Authentication.js
+++ frontend/src/Authentication.js
@@ -23,49 +23,53 @@ class Authentication extends Component {
         event.preventDefault();
     }
 
-    loginView() {
-        return (
-            <div >
-                <h2>Login</h2>
-                <div className="Login">
-                    <form onSubmit={this.handleSubmit}>
-                        <label htmlFor="username" >Username</label>
-                        <input type="text" name="username" value={this.state.username} onChange={(event) => this.setState({ username: event.target.value })} />
-                        <label htmlFor="password" >Password</label>
-                        <input type="password" name="password" value={this.state.password1} onChange={(event) => this.setState({ password1: event.target.value })} />
-                        <button disabled={!this.validateLoginForm()} onClick={() => authService.loginToken(this.state)}>Login</button>
-                        <button onClick={(event) => this.setState({ register: true })}>Register</button>
-                    </form>
-                </div>
-            </div >
-        )
+    renderView() {
+        if(!this.state.register)
+        {
+            return (
+                <div >
+                    <h2>Login</h2>
+                    <div className="Login">
+                        <form onSubmit={this.handleSubmit}>
+                            <label htmlFor="username" >Username</label>
+                            <input type="text" name="username" value={this.state.username} onChange={(event) => this.setState({ username: event.target.value })} />
+                            <label htmlFor="password" >Password</label>
+                            <input type="password" name="password" value={this.state.password1} onChange={(event) => this.setState({ password1: event.target.value })} />
+                            <button disabled={!this.validateLoginForm()} onClick={() => authService.loginToken(this.state)}>Login</button>
+                            <button onClick={(event) => this.setState({ register: true })}>Register</button>
+                        </form>
+                    </div>
+                </div >
+            )
+        }else
+        {
+            return (
+                <div >
+                    <h2>Register!</h2>
+                    <div className="Login">
+                        <form onSubmit={this.handleSubmit}>
+                            <label htmlFor="username" >Username</label>
+                            <input type="text" name="username" value={this.state.username} onChange={(event) => this.setState({ username: event.target.value })} />
+                            <label htmlFor="email" >Email</label>
+                            <input type="text" name="email" value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
+                            <label htmlFor="password" >Password</label>
+                            <input type="password" name="password1" value={this.state.password1} onChange={(event) => this.setState({ password1: event.target.value })} />
+                            <label htmlFor="Repeat password" >Repeat Password</label>
+                            <input type="password" name="password2" value={this.state.password2} onChange={(event) => this.setState({ password2: event.target.value })} />
+                            <button disabled={!this.validateRegisterForm()} onClick={() => authService.registerToken(this.state)}>Register</button>
+                            <button onClick={(event) => this.setState({ register: false })}>Return</button>
+                        </form>
+                    </div>
+                </div >
+            )
+        }
+        
     }
-    registerView() {
-        return (
-            <div >
-                <h2>Register!</h2>
-                <div className="Login">
-                    <form onSubmit={this.handleSubmit}>
-                        <label htmlFor="username" >Username</label>
-                        <input type="text" name="username" value={this.state.username} onChange={(event) => this.setState({ username: event.target.value })} />
-                        <label htmlFor="email" >Email</label>
-                        <input type="text" name="email" value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
-                        <label htmlFor="password" >Password</label>
-                        <input type="password" name="password1" value={this.state.password1} onChange={(event) => this.setState({ password1: event.target.value })} />
-                        <label htmlFor="Repeat password" >Repeat Password</label>
-                        <input type="password" name="password2" value={this.state.password2} onChange={(event) => this.setState({ password2: event.target.value })} />
-                        <button disabled={!this.validateRegisterForm()} onClick={() => authService.registerToken(this.state)}>Register</button>
-                        <button onClick={(event) => this.setState({ register: false })}>Return</button>
-                    </form>
-                </div>
-            </div >
-        )
-    }
+
     render() {
         return (
             <div>
-                {this.state.register && this.registerView()}
-                {!this.state.register && this.loginView()}
+                {this.renderView()}
             </div>
         )
     }
