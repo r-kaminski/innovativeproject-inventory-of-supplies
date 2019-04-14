90b78491028c34d4164d29987ee8f26520cf3f42
Rafal Kaminski <rkam456@gmail.com>
2019-04-14 14:55:54 +0200

    Views and urls for inventories app added

--- backend/inventories/urls.py
+++ backend/inventories/urls.py
@@ -1,31 +1,13 @@
 from django.urls import path
-from .views import InventoryReportCreateView
+from .views import InventoryReportListView, InventoryReportCreateView, InventoryReportDetailsView, InventoryReportRemoveView, InventorySupplyDetailsUpdateView
+
+
+app_name = "inventories"
 
 urlpatterns = [
-    path('create', InventoryReportCreateView.as_view())
+    path('', InventoryReportListView.as_view()),
+    path('<int:pk>', InventoryReportDetailsView.as_view(), name='report-details'),
+    path('create', InventoryReportCreateView.as_view()),
+    path('remove/<int:pk>', InventoryReportRemoveView.as_view(), name='report-remove'),
+    path('supplies/<int:pk>', InventorySupplyDetailsUpdateView.as_view(), name='supplies-details-update'), 
 ]
-# /api/inventories/ in backend.urls
-
-# '/reports' - list InventoryReport objects (List)
-
-# '/reports/<int:pk>' - details of particular InventoryReport object (Details)
-#   List of assigned InventorySupply objects can be accesed by 'supplies' attribute
-#   RetrieveUpdateDestroyAPIView for admin user
-#   RetrieveAPIView for other users
-#   Most likely scanning QR codes and manual ID input should be handled here on client side
-
-# '/reports/create' - create InventoryReport object (Create)
-#   Whenever admin creates new report, it should be populated with supplies existing in database
-#   Therefore create method for this view should be overrided, and population flow provided
-
-# '/reports/<int:pk>/supplies/checkout'
-#   a supplies.Supply id is provided in request
-#   id can be used to map to InventorySupply
-#   e.g.: InventorySupply.objects.filter(inventory_report__id=ir_ID).filter(inventory_supply__id=is_ID)[0]
-
-# '/supplies/<int:pk>' - view or update (status of) particular InventorySupply (RetrieveUpdate)
-#   Update should only handle setting is_checked flag on or off
-#   Other modifications shouldn't be allowed there
-#   inventories.InventorySupply maps to supplies.Supply so modificating latter would affect the former
-#   Population (creation) of inventories.InventorySupply will be handled through InventoryReport creation view
-
--- backend/inventories/views.py
+++ backend/inventories/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status, viewsets
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
-from .models import InventoryReport
-from .serializers import InventoryReportSerializer
+from .models import InventoryReport, InventorySupply
+from .serializers import InventoryReportSerializer, InventorySupplySerializer
+
+
 
 class InventoryReportCreateView(generics.CreateAPIView):
     permission_classes = (permissions.IsAdminUser,)
     queryset = InventoryReport.objects.all()
     serializer_class = InventoryReportSerializer
+
+
+class InventoryReportListView(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = InventoryReport.objects.all()
+    serializer_class = InventoryReportSerializer
+
+
+class InventoryReportDetailsView(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = InventoryReport.objects.all()
+    serializer_class = InventoryReportSerializer
+
+
+class InventoryReportRemoveView(generics.DestroyAPIView):
+    permission_classes = (permissions.IsAdminUser,)
+    queryset = InventoryReport.objects.all()
+    serializer_class = InventoryReportSerializer
+
+
+class InventorySupplyDetailsUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = InventorySupply.objects.all()
+    serializer_class = InventorySupplySerializer
