a43108d7dfd6532793bca353339e0173c6aa38f4
KBialecki <36888024+KBialecki@users.noreply.github.com>
2019-04-17 22:22:21 +0200

    removed unnecessary file

--- backend/printing/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import QRCodePrint
-
-
-class QRPrintSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = QRCodePrint
-        fields = '__all__'
