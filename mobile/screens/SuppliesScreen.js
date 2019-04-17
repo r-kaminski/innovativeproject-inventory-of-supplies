d61737adf5f5be581a23ffc48ef24f4f6d98cb0f
Julia Zajusz <julia.zajusz@fingo.pl>
2019-04-17 19:14:27 +0200

    restore mobile app

---
+++ mobile/screens/SuppliesScreen.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {RefreshControl, ScrollView, StyleSheet, Text} from 'react-native';
+import {getSupplies} from "../services/SuppliesService";
+import {Button, ListItem} from "react-native-elements";
+
+export default class SuppliesScreen extends React.Component {
+
+    state = {
+        isShowingText: true,
+        "count": 0,
+        "next": null,
+        "previous": null,
+        "results": [],
+        "total_pages": 1,
+        refreshing: false,
+    };
+
+    componentDidMount() {
+        this._onRefresh()
+    }
+
+    _onRefresh = () => {
+        this.setState({refreshing: true});
+        this.fetchData().then(() => {
+            this.setState({refreshing: false});
+        });
+    }
+
+    async fetchData() {
+        await getSupplies({page: 1, page_size: 10}).then((res) => {
+            {
+                this.setState(res)
+            }
+        })
+    }
+
+
+    static navigationOptions = {
+        header: null
+    };
+
+    render() {
+        return (
+            <ScrollView style={styles.container}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={this.state.refreshing}
+                                onRefresh={this._onRefresh}
+                            />
+                        }
+            >
+
+                {this.state.results.map((supply, index) => {
+                    return <ListItem
+                        style={styles.listItem}
+                        key={index}
+                        leftAvatar={{source: {uri: 'https://via.placeholder.com/150'}}}
+                        title={supply.name}
+                        subtitle={
+                            <Text style={styles.subtitle}
+                                  ellipsizeMode={'tail'}
+                                  numberOfLines={1}
+                            >{supply.description}</Text>
+                        }
+
+                        // subtitle={supply.description}
+                        onPress={() => this._handlePressTool(supply.id)}
+                    />
+                })}
+
+                <Button onPress={() => this.onPressNavigateToAddNewSupply()} title={"Dodaj nowe narządzie"}
+                        buttonStyle={{backgroundColor: "#40c1ac"}}/>
+
+            </ScrollView>
+        );
+    }
+
+    onPressNavigateToAddNewSupply = () => {
+        const {navigate} = this.props.navigation;
+        navigate('SupplyAdd', {onRefresh: () => this._onRefresh()})
+    }
+
+    _handlePressTool = (id) => {
+        const {navigate} = this.props.navigation;
+        navigate('Supply', {id: id})
+    };
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        borderColor: 'red',
+        borderWidth: 1,
+    },
+    listItem: {
+        borderBottomWidth: 1,
+        borderColor: '#d0d0d0'
+    },
+    subtitle: {
+        color: '#d0d0d0',
+
+    }
+});
---
+++ mobile/screens/SupplyScreen.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import {Button, Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {getSupply} from "../services/SuppliesService";
+
+
+export default class SupplyScreen extends React.Component {
+
+    state = {
+        isShowingText: true,
+        supply: null
+    };
+
+    componentDidMount() {
+        this.reload();
+    }
+
+
+    reload() {
+        getSupply(this.props.navigation.getParam("id")).then((res) => {
+            {
+                this.setState({supply: res})
+            }
+        })
+    }
+
+    static navigationOptions = {
+        header: null
+    };
+
+    onPressEditTool = () => {
+        const {navigate} = this.props.navigation;
+        navigate('SupplyEdit', {supply: this.state.supply, onGoBack: () => this.reload()})
+    }
+
+    render() {
+        const {supply} = this.state
+        return (
+            <View style={styles.container}>
+                <ScrollView style={styles.container}>
+                    {this.state.supply && <View style={styles.mainView}>
+                        <View style={styles.imageContainer}>
+                            <Image
+                                source={{uri: 'https://via.placeholder.com/150'}}
+                                resizeMode="contain"
+                                fadeDuration={0}
+                                style={styles.image}
+                            />
+                        </View>
+                        <Text style={styles.optionsTitleText}>
+                            {supply.name}
+                        </Text>
+                        <View style={styles.row}>
+
+                            <View style={{paddingRight: 8}}>
+                                <Text style={styles.descriptionText}>
+                                    id:
+                                </Text>
+
+                                <Text style={styles.descriptionText}>
+                                    stan:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                    opis:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                    {supply.localization}
+                                </Text>
+                            </View>
+                            <View>
+                                <Text style={styles.optionText}>
+                                    {supply.id}
+                                </Text>
+
+                                <Text style={styles.optionText}>
+                                    {supply.state}
+                                </Text>
+                                <Text style={styles.optionText}>
+                                    {supply.description}
+                                </Text>
+                                <Text style={styles.optionText}>
+                                    {supply.localization}
+                                </Text>
+                            </View>
+
+
+                        </View>
+                    </View>}
+
+                </ScrollView>
+                <Button onPress={() => this.onPressEditTool()} title={"Edytuj"}
+                        color={"#098584"}/>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    mainView: {
+        paddingRight: 20,
+        paddingLeft: 20
+    },
+    imageContainer: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // borderColor: 'red',
+        // borderBottomWidth: 1,
+        padding: 20
+    },
+    image: {
+        width: 150,
+        height: 150,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        maxHeight: "100%"
+    },
+    optionsTitleText: {
+        fontSize: 18,
+        marginBottom: 12,
+    },
+    optionText: {
+        fontSize: 15,
+        marginTop: 1,
+    },
+    descriptionText: {
+        fontSize: 15,
+        marginTop: 1,
+        color: '#808080'
+    },
+    row: {
+        flexDirection: 'row',
+    }
+});
