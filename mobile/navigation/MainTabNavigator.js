01013b1d2b087b80ef948cfbda4ca040503f14f4
Marek Pokropiński <marek.pokropinski@outlook.com>
2019-05-01 18:11:40 +0200

    Change amount of spaces.

--- mobile/navigation/MainTabNavigator.js
+++ mobile/navigation/MainTabNavigator.js
@@ -15,63 +15,63 @@ import StocktakingAdd from "../screens/StocktakingAdd";
 
 
 const StocktakingStack = createStackNavigator({
-  StocktakingsScreen: StocktakingsScreen,
-  StocktakingScreen: StocktakingScreen,
-  StocktakingAdd: StocktakingAdd,
-  StocktakingScanner: ScanScreen,
+    StocktakingsScreen: StocktakingsScreen,
+    StocktakingScreen: StocktakingScreen,
+    StocktakingAdd: StocktakingAdd,
+    StocktakingScanner: ScanScreen,
 });
 
 StocktakingStack.navigationOptions = {
-  tabBarLabel: 'Stocktaking',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-albums${focused ? '' : '-outline'}`
-          : 'md-albums'
-      }
-    />
-  ),
+    tabBarLabel: 'Stocktaking',
+    tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+            focused={focused}
+            name={
+                Platform.OS === 'ios'
+                    ? `ios-albums${focused ? '' : '-outline'}`
+                    : 'md-albums'
+            }
+        />
+    ),
 };
 
 const ToolsStack = createStackNavigator({
-  Supplies: SuppliesScreen,
-  Supply: { screen: SupplyScreen },
-  SupplyEdit: { screen: SupplyEdit },
-  SupplyAdd: { screen: SupplyAdd },
+    Supplies: SuppliesScreen,
+    Supply: { screen: SupplyScreen },
+    SupplyEdit: { screen: SupplyEdit },
+    SupplyAdd: { screen: SupplyAdd },
 });
 
 ToolsStack.navigationOptions = {
-  tabBarLabel: 'Narzędzia',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-hammer${focused ? '' : '-outline'}`
-          : 'md-hammer'
-      }
-    />
-  ),
+    tabBarLabel: 'Narzędzia',
+    tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+            focused={focused}
+            name={
+                Platform.OS === 'ios'
+                    ? `ios-hammer${focused ? '' : '-outline'}`
+                    : 'md-hammer'
+            }
+        />
+    ),
 };
 
 const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+    Settings: SettingsScreen,
 });
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
+    tabBarLabel: 'Settings',
+    tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+        />
+    ),
 };
 
 export default createBottomTabNavigator({
-  ToolsStack,
-  StocktakingStack,
-  SettingsStack
+    ToolsStack,
+    StocktakingStack,
+    SettingsStack
 });
