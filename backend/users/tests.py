3869cd898796c9ee67543a65a0e0819de199bf57
Marek Pokropiński <marek.pokropinski@outlook.com>
2019-05-03 18:49:50 +0200

    Add endpoint to get current user info at /api/users/whoami. Render buttons to for admin actions only if current user is admin.

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -78,3 +78,28 @@ class UserTests(APITestCase):
         response = UserListView.as_view()(request)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_current_user_authenticated(self):
+        """
+        Test getting current user data.
+        Authentication is provided
+        """
+        request = self.factory.get('/api/users/whoami')
+        force_authenticate(request, user=self.testuser)
+        response = UserDetailsView.as_view()(request, pk=self.testuser.pk)
+
+        # compare if response matches with user data
+        for key, value in response.data.items():
+            self.assertEqual(value, vars(request.user)[key])
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_current_user_unauthenticated(self):
+        """
+        Test getting current user data.
+        Authentication is not provided
+        """
+        request = self.factory.get('/api/users/current')
+        response = UserDetailsView.as_view()(request, pk=None)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from django.conf.urls import url
 
-from .views import UserListView, UserDetailsView
+from .views import UserListView, UserDetailsView, RetrieveCurrentUserView
 
 app_name = "users"
 
 urlpatterns = [
     path('', UserListView.as_view(), name="user-list"),
     path('<pk>/', UserDetailsView.as_view(), name="user-details"),
+    path('whoami', RetrieveCurrentUserView.as_view(),
+         name="current-user-details"),
 ]
