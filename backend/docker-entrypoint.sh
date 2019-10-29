3b5eba24a3dea359d1a636e1d93ee3cfe4f13904
r-kaminski <rkam456@gmail.com>
2019-10-29 22:02:40 +0100

    Properly verify database health before starting Django

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
-sleep 1
+echo "Waiting for $DB_HOST"
+while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 0.1
+done
+echo "$DB_HOST ready"
 
 python manage.py makemigrations
 python manage.py migrate
