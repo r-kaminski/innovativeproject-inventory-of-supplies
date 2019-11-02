873db6e78e83e4f3a147f74affc56724db606704
Rafal Kaminski <31936384+r-kaminski@users.noreply.github.com>
2019-11-02 15:08:23 +0100

    Don't execute extra commands in entrypoint while testing

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for $DB_HOST"
-while ! nc -z $DB_HOST $DB_PORT; do
-    sleep 0.1
-done
-echo "$DB_HOST ready"
+if [[ ! "${DJANGO_SETTINGS_MODULE}" =~ "testing" ]]; then
+    echo "Waiting for $DB_HOST"
+    while ! nc -z $DB_HOST $DB_PORT; do
+        sleep 0.1
+    done
+    echo "$DB_HOST ready"
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py shell < create-superuser.py
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py shell < create-superuser.py
+fi
 
 exec "$@"
